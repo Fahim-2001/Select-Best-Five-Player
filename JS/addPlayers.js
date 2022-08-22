756aa32981c05a3258b4f78703681fde111dc99d
unknown <mdyasirofficial2001@gmail.com>
2022-08-23 03:41:21 +0600

    JS file added for listing players

---
+++ JS/addPlayers.js
@@ -0,0 +1,16 @@
+function addPlayerName(element) {
+  const playerName =
+    element.parentNode.parentNode.children[1].children[0].innerText;
+
+  const playerListElement = document.getElementById("players-list");
+  const newPlayer = document.createElement("li");
+  newPlayer.innerText = playerName;
+  playerListElement.appendChild(newPlayer);
+
+  const listSize = document.getElementById("players-list").childNodes;
+  const numberOfChilds = listSize.length;
+  if (numberOfChilds > 5) {
+    alert("Select limit is up. You Cannot select more than FIVE players");
+  }
+  onclick = element.disabled = "true";
+}
